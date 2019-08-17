7a010ee6819a3f824f42c8874d2cf67fdd821d07
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-17 15:00:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,10 @@ def take_a_number(katz_deli, name)
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving
-
+def now_serving(array)
+  if array == 0
+    puts "There is nobody waiting to be served!"
+  else
+    
+  end
 end
