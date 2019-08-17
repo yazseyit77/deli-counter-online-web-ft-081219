3feed42e91e77bbc45b67ae1a28de9cc208ad065
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-17 14:57:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,8 +7,9 @@ def line(x_line)
     x_line.each.with_index(1) do |name, index|
       array.push("#{index}. #{name}")
   end
-end
   puts "The line is currently: #{array.join(" ")}"
+end
+  
 end
 
 def take_a_number(katz_deli, names)
