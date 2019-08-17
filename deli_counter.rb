d7dccbcd37b3eaf3fcb6910ba73951f4b55c4de2
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-17 14:43:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 def line(katz_deli)
   if katz_deli.length > 1
     string = "The line is currently:"
-    katz_deli.each {|val, index| output << " #{index+1}. #{val}"} 
+    katz_deli.each {|val, index| output << " #{index+1}. #{val}"}
     puts string
   else
     "The line is currently empty"
